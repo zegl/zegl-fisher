0b59c848def36a106b7f40e0f69b725f0e4b117b
Gustav Westling <gustav@polar.sh>
2023-04-18 09:47:14 +0200

    zpr, zrb

--- functions/zegl.fish
+++ functions/zegl.fish
@@ -1,7 +1,13 @@
-function prx
+function zpr
     git fetch origin
     git rebase -i origin/main --autosquash
     git push -u origin HEAD --force
     gh pr create --fill
     gh pr merge --auto
+end
+
+
+function zrb
+    git fetch origin
+    git rebase -i origin/main --autosquash
 end
