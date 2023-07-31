44c532662bbdebaab5af7f05d3e51412afe7a066
Gustav Westling <gustav@polar.sh>
2023-07-31 13:07:58 +0200

    Add znofity

--- functions/zegl.fish
+++ functions/zegl.fish
@@ -23,3 +23,11 @@ end
 function zfup
     git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup
 end
+
+
+function znotify --description "znotify <title> <message>"
+    set title $argv[1]
+    set --erase argv[1]
+    curl -X POST "https://sn.30cm.org/v1/push" -H 'Content-Type: application/json' -d '{"auth":"'"$SN_30CM_ORG_AUTH"'","message":"'"$argv"'", "title":"'"$title"'"}'
+    echo "Sent! 🔔"
+end
