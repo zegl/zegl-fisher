d5a584ae7049f4e76994033f9d778fc2a977e40c
Gustav Westling <gustav@sanalabs.com>
2024-08-01 17:36:06 +0200

    zfuture

--- functions/zegl.fish
+++ functions/zegl.fish
@@ -37,6 +37,52 @@ function zpp
     git push -u origin HEAD --force
 end
 
+function zfuture
+    set trunkname $(ztrunkname)
+    set ghusername $(gh api user | jq -r '.login')
+
+    # if on branch tmp_future, warn and exit
+    if test (git branch --show-current) = "tmp_future"
+        echo "You are on the tmp_future branch, please switch to another branch before running this script"
+        exit 1
+    end
+
+    set_color green; echo "Z: Preparing..."
+    set_color normal;
+
+    git branch -D tmp_future
+    git checkout -b tmp_future
+    git fetch origin
+    git reset --hard $trunkname
+
+    # All heads that are not merged into the trunk
+    set unmerged_ids $(gh pr list --author "$ghusername" --json headRefOid --jq '.[] | .headRefOid')
+
+    for id in $unmerged_ids
+        set_color green; echo "Z: Applying PR $id"
+        set_color normal;
+
+        set pr_commits $(git log --oneline $trunkname..$id)
+
+        for commit in $pr_commits
+            set commit_hash (echo $commit | awk '{print $1}')
+
+            set short (git log --pretty=oneline --abbrev-commit -n1 $commit_hash)
+
+            set_color green; echo "Z: Cherry Picking $short"
+            set_color normal;
+
+            git cherry-pick $commit_hash
+        end
+    end
+
+    git branch -D future
+    git checkout -b future
+    git branch -D tmp_future
+
+    set_color green; echo "Z: All done, welcome to the future!"
+    set_color normal;
+end
 
 function znotify --description "znotify <title> <message>"
     set title $argv[1]
